9bb412a16afe186ab5c56cfd508acbf74b851ebb
leanghok120 <leanghokoeng5@gmail.com>
2023-10-28 06:57:12 +0700

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let newStr = String(str);
+  let reversedStr = newStr.split('').reverse().join('');
 
+  return reversedStr;
 };
 
 // Do not edit below this line
