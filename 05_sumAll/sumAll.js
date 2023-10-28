9cea3a59ed2c7619e184b8f77eedd13ab3c9ef8e
leanghok120 <leanghokoeng5@gmail.com>
2023-10-28 08:41:37 +0700

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  let result = 0;
 
+  if (firstNumber > secondNumber) {
+    [firstNumber, secondNumber] = [secondNumber, firstNumber]
+  } else if (firstNumber < 0) {
+      return "ERROR";
+  } else if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+    return "ERROR";
+  } 
+  
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
